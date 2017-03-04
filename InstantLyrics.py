98a8a1cc9d0221c93f0404736643417d9e9e9ee6
Bhrigu <captain.bhrigu@gmail.com>
2017-03-04 17:24:51 +0530

    error on spotify song not found

--- InstantLyrics.py
+++ InstantLyrics.py
@@ -52,7 +52,7 @@ class LyricsWindow(Gtk.Window):
 
 
 		self.lyrics_vbox.pack_start(self.title, False, False, 5)
-		self.lyrics_vbox.pack_start(self.lyrics, True, True, 5)
+		self.lyrics_vbox.pack_start(self.lyrics, False, False, 5)
 		self.lyrics_vbox.set_size_request(350, 700)
 
 		self.main_box.pack_start(self.lyrics_vbox, True, True, 0)
@@ -78,25 +78,26 @@ class LyricsWindow(Gtk.Window):
 
 	def get_spotify_song_data(self):
 		session_bus = dbus.SessionBus()
-		try:
-			spotify_bus = session_bus.get_object("org.mpris.MediaPlayer2.spotify","/org/mpris/MediaPlayer2")
-			spotify_properties = dbus.Interface(spotify_bus,"org.freedesktop.DBus.Properties")
-			metadata = spotify_properties.Get("org.mpris.MediaPlayer2.Player", "Metadata")
-		except:
-			# I don't know what to do now
-			return {'title':"Budapest", 'artist':"George Ezra",
-					'artUrl':"https://open.spotify.com/image/191168d7b8c69c33910d65116c7b0315e38b5e6c" }
-
+		
+		spotify_bus = session_bus.get_object("org.mpris.MediaPlayer2.spotify","/org/mpris/MediaPlayer2")
+		spotify_properties = dbus.Interface(spotify_bus,"org.freedesktop.DBus.Properties")
+		metadata = spotify_properties.Get("org.mpris.MediaPlayer2.Player", "Metadata")
+	
 		title = metadata['xesam:title'].encode('utf-8')
 		artist = metadata['xesam:artist'][0].encode('utf-8')
 		return {'title':title, 'artist':artist,'artUrl':str(metadata['mpris:artUrl']) }
 
 	def get_spotify(self):
 
-		song_data = self.get_spotify_song_data()
-		song = song_data['title']
-		artist = song_data['artist']
-		artUrl = song_data['artUrl']
+		try:
+			song_data = self.get_spotify_song_data()
+			song = song_data['title']
+			artist = song_data['artist']
+			artUrl = song_data['artUrl']
+		except:
+			self.title.set_markup("<big><b>Error</b></big>")
+			self.lyrics.set_text("Could not get current spotify song details")
+			return
 
 		title = "<b><big>" + song + "</big>\n" + artist + "</b>"
 		self.title.set_markup(title)
