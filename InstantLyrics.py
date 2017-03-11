8f89f9a2943ddc27411b6fcd3608b6b5f1144062
Bhrigu <captain.bhrigu@gmail.com>
2017-03-11 13:54:13 +0530

    return key; code modular

--- InstantLyrics.py
+++ InstantLyrics.py
@@ -1,7 +1,7 @@
 import gi
 gi.require_version('Gtk', '3.0')
 gi.require_version('AppIndicator3', '0.1')
-from gi.repository import Gtk
+from gi.repository import Gtk, Gdk
 from gi.repository import AppIndicator3 as appindicator
 
 import os
@@ -11,13 +11,13 @@ import time
 import dbus
 from Lyrics import get_lyrics
 
-APPINDICATOR_ID = 'myappindicator'
-
+APPINDICATOR_ID = 'lyricsappindicator'
 
 class LyricsWindow(Gtk.Window):
 
 	def __init__(self, type):
 		Gtk.Window.__init__(self, title="Lyrics")
+		self.set_icon_from_file(os.path.abspath('icon.svg'))
 		self.set_border_width(20)
 		self.set_default_size(350, 700)
 		self.set_position(Gtk.WindowPosition.CENTER)
@@ -26,22 +26,40 @@ class LyricsWindow(Gtk.Window):
 		self.main_box.set_size_request(350, 700)
 		
 		if(type == "get"):
-
-			entry_hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=50)
-			entry_hbox.set_property("margin", 10)
-
-			self.input = Gtk.Entry()
-			self.input.set_text("Enter song/artist")
-			entry_hbox.pack_start(self.input, True, True, 0)
-
-			submit = Gtk.Button.new_with_label("Get Lyrics")
-			submit.connect("clicked", self.fetch_lyrics)
-			entry_hbox.pack_start(submit, True, True, 0)
-
+			entry_hbox = self.create_input_box()
 			self.main_box.pack_start(entry_hbox, False, False, 10)
 
+		lyrics_vbox = self.create_lyrics_box()
+		self.main_box.pack_start(lyrics_vbox, True, True, 0)
 
-		self.lyrics_vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+		scrolled = Gtk.ScrolledWindow()
+		scrolled.set_policy(Gtk.PolicyType.NEVER, Gtk.PolicyType.AUTOMATIC)
+		scrolled.add(self.main_box)
+
+		self.add(scrolled)
+		self.show_all()
+
+	def on_key_release(self, widget, ev, data=None):
+		if ev.keyval == Gdk.KEY_Return:
+			self.fetch_lyrics()
+
+	def create_input_box(self):
+		entry_hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=50)
+		entry_hbox.set_property("margin", 10)
+
+		self.input = Gtk.Entry()
+		self.input.set_text("song/artist/whatever")
+		self.input.connect("key-release-event", self.on_key_release)
+		entry_hbox.pack_start(self.input, True, True, 0)
+
+		submit = Gtk.Button.new_with_label("Get Lyrics")
+		submit.connect("clicked", self.fetch_lyrics)
+		entry_hbox.pack_start(submit, True, True, 0)
+
+		return entry_hbox
+
+	def create_lyrics_box(self):
+		lyrics_vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
 
 		self.title = Gtk.Label()
 		self.title.set_justify(Gtk.Justification.CENTER)
@@ -54,31 +72,24 @@ class LyricsWindow(Gtk.Window):
 
 		self.spinner = Gtk.Spinner()
 
-		self.lyrics_vbox.pack_start(self.title, False, False, 5)
-		self.lyrics_vbox.pack_start(self.spinner, False, False, 5)
-		self.lyrics_vbox.pack_start(self.lyrics, False, False, 5)
-		self.lyrics_vbox.set_size_request(350, 700)
+		lyrics_vbox.pack_start(self.title, False, False, 5)
+		lyrics_vbox.pack_start(self.spinner, False, False, 5)
+		lyrics_vbox.pack_start(self.lyrics, False, False, 5)
+		lyrics_vbox.set_size_request(350, 700)
 
-		self.main_box.pack_start(self.lyrics_vbox, True, True, 0)
+		return lyrics_vbox
 
-		scrolled = Gtk.ScrolledWindow()
-		scrolled.set_policy(Gtk.PolicyType.NEVER, Gtk.PolicyType.AUTOMATIC)
-		scrolled.add(self.main_box)
-
-		self.add(scrolled)
-		self.show_all()
-
-			
 	def put_lyrics(self, song):
 		self.spinner.start()
+
 		self.lyrics.set_text("")
 		lyrics = get_lyrics(song)
 		self.lyrics.set_text(lyrics)
+
 		self.spinner.stop()
 
-	def fetch_lyrics(self, source):
+	def fetch_lyrics(self, source=None):
 		input = self.input.get_text()
-
 		text = "<b><big>"+input+"</big></b>"
 		self.title.set_markup(text)
 
@@ -93,9 +104,9 @@ class LyricsWindow(Gtk.Window):
 		spotify_properties = dbus.Interface(spotify_bus,"org.freedesktop.DBus.Properties")
 		metadata = spotify_properties.Get("org.mpris.MediaPlayer2.Player", "Metadata")
 	
-		title = metadata['xesam:title'].encode('utf-8')
-		artist = metadata['xesam:artist'][0].encode('utf-8')
-		return {'title':title, 'artist':artist,'artUrl':str(metadata['mpris:artUrl']) }
+		title = metadata['xesam:title'].encode('utf-8').replace("&", "&amp;")
+		artist = metadata['xesam:artist'][0].encode('utf-8').replace("&", "&amp;")
+		return {'title':title, 'artist':artist}
 
 	def get_spotify(self):
 
@@ -103,9 +114,7 @@ class LyricsWindow(Gtk.Window):
 			song_data = self.get_spotify_song_data()
 			song = song_data['title']
 			artist = song_data['artist']
-			artUrl = song_data['artUrl']
 		except:
-			self.lyrics_vbox.remove(self.spinner)
 			self.title.set_markup("<big><b>Error</b></big>")
 			self.lyrics.set_text("Could not get current spotify song details")
 			return
@@ -114,14 +123,19 @@ class LyricsWindow(Gtk.Window):
 		self.title.set_markup(title)
 
 		self.put_lyrics(song + " " + artist)
-		#self.lyrics_vbox.remove(self.spinner)
+
+	def get_resource_path(rel_path):
+		dir_of_py_file = os.path.dirname(__file__)
+		rel_path_to_resource = os.path.join(dir_of_py_file, rel_path)
+		abs_path_to_resource = os.path.abspath(rel_path_to_resource)
+		return abs_path_to_resource
 
 class AppIndicator():
 	
 	def __init__(self):
 		signal.signal(signal.SIGINT, signal.SIG_DFL)
 
-		indicator = appindicator.Indicator.new(APPINDICATOR_ID, os.path.abspath('lyrics.svg'), appindicator.IndicatorCategory.SYSTEM_SERVICES)
+		indicator = appindicator.Indicator.new(APPINDICATOR_ID, os.path.abspath('icon.svg'), appindicator.IndicatorCategory.SYSTEM_SERVICES)
 		indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
 		indicator.set_menu(self.build_menu())
 		Gtk.main()
--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # Instant-Lyrics
+
+Get lyrics of the currently playing spotify song, or any song, instantly.
+
+A linux application with a very convinient GUI. Build with Python Gtk+3 (gi).
+
+# Screenshots
+
+# Requirements
+
+* python-gi
+
+* python3-gi
+
+* dbus
+
+You can install these for your system either by `pip` or your package manager.
+
+Like, for Ubuntu/Debian based OS, you can install as:
+
+`sudo apt install python-gi`
+`sudo apt install python3-gi`
+`sudo apt install python-dbus`
+
+# Installation
+
+## From source
+
+1. Clone the repository. `git clone https://github.com/bhrigu123/Instant-Lyrics.git`
+
+2. Run `python InstantLyrics.py`
+
+The icon will appear in the task menu now.
+
