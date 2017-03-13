5cee5e89b7ef2153150bcf4e2f92998504c2af7d
Bhrigu <captain.bhrigu@gmail.com>
2017-03-13 17:38:13 +0530

    PEP8; lyrics not found

--- InstantLyrics.py
+++ InstantLyrics.py
@@ -13,171 +13,182 @@ from Lyrics import get_lyrics
 
 APPINDICATOR_ID = 'lyricsappindicator'
 
+
 def get_icon_path(rel_path):
-	dir_of_py_file = os.path.dirname(__file__)
-	rel_path_to_resource = os.path.join(dir_of_py_file, rel_path)
-	abs_path_to_resource = os.path.abspath(rel_path_to_resource)
-	return abs_path_to_resource
+    dir_of_py_file = os.path.dirname(__file__)
+    rel_path_to_resource = os.path.join(dir_of_py_file, rel_path)
+    abs_path_to_resource = os.path.abspath(rel_path_to_resource)
+    return abs_path_to_resource
+
 
 class LyricsWindow(Gtk.Window):
 
-	def __init__(self, type):
-		Gtk.Window.__init__(self, title="Lyrics")
-		self.set_icon_from_file(get_icon_path('icon.svg'))
-		self.set_border_width(20)
-		self.set_default_size(350, 650)
-		self.set_position(Gtk.WindowPosition.CENTER)
+    def __init__(self, type):
+        Gtk.Window.__init__(self, title="Lyrics")
+        self.set_icon_from_file(get_icon_path('icon.svg'))
+        self.set_border_width(20)
+        self.set_default_size(350, 650)
+        self.set_position(Gtk.WindowPosition.CENTER)
 
-		self.main_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
-		self.main_box.set_size_request(350, 700)
-		
-		if(type == "get"):
-			entry_hbox = self.create_input_box()
-			self.main_box.pack_start(entry_hbox, False, False, 10)
+        self.main_box = Gtk.Box(
+            orientation=Gtk.Orientation.VERTICAL, spacing=6)
+        self.main_box.set_size_request(350, 700)
 
-		lyrics_vbox = self.create_lyrics_box()
-		self.main_box.pack_start(lyrics_vbox, True, True, 0)
+        if(type == "get"):
+            entry_hbox = self.create_input_box()
+            self.main_box.pack_start(entry_hbox, False, False, 10)
 
-		scrolled = Gtk.ScrolledWindow()
-		scrolled.set_policy(Gtk.PolicyType.NEVER, Gtk.PolicyType.AUTOMATIC)
-		scrolled.add(self.main_box)
+        lyrics_vbox = self.create_lyrics_box()
+        self.main_box.pack_start(lyrics_vbox, True, True, 0)
 
-		self.add(scrolled)
-		self.show_all()
+        scrolled = Gtk.ScrolledWindow()
+        scrolled.set_policy(Gtk.PolicyType.NEVER, Gtk.PolicyType.AUTOMATIC)
+        scrolled.add(self.main_box)
 
-	def on_key_release(self, widget, ev, data=None):
-		if ev.keyval == Gdk.KEY_Return:
-			self.fetch_lyrics()
+        self.add(scrolled)
+        self.show_all()
 
-	def create_input_box(self):
-		entry_hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=50)
-		entry_hbox.set_property("margin", 10)
+    def on_key_release(self, widget, ev, data=None):
+        if ev.keyval == Gdk.KEY_Return:
+            self.fetch_lyrics()
 
-		self.input = Gtk.Entry()
-		self.input.set_text("song/artist")
-		self.input.connect("key-release-event", self.on_key_release)
-		entry_hbox.pack_start(self.input, True, True, 0)
+    def create_input_box(self):
+        entry_hbox = Gtk.Box(
+            orientation=Gtk.Orientation.HORIZONTAL, spacing=50)
+        entry_hbox.set_property("margin", 10)
 
-		submit = Gtk.Button.new_with_label("Get Lyrics")
-		submit.connect("clicked", self.fetch_lyrics)
-		entry_hbox.pack_start(submit, True, True, 0)
+        self.input = Gtk.Entry()
+        self.input.set_text("song/artist")
+        self.input.connect("key-release-event", self.on_key_release)
+        entry_hbox.pack_start(self.input, True, True, 0)
 
-		return entry_hbox
+        submit = Gtk.Button.new_with_label("Get Lyrics")
+        submit.connect("clicked", self.fetch_lyrics)
+        entry_hbox.pack_start(submit, True, True, 0)
 
-	def create_lyrics_box(self):
-		lyrics_vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+        return entry_hbox
 
-		self.title = Gtk.Label()
-		self.title.set_justify(Gtk.Justification.CENTER)
+    def create_lyrics_box(self):
+        lyrics_vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
 
-		self.lyrics = Gtk.Label()
-		self.lyrics.set_justify(Gtk.Justification.CENTER)
-		self.lyrics.set_property("margin_left", 40)
-		self.lyrics.set_property("margin_right", 40)
-		self.lyrics.set_line_wrap(True)
+        self.title = Gtk.Label()
+        self.title.set_justify(Gtk.Justification.CENTER)
 
-		self.spinner = Gtk.Spinner()
+        self.lyrics = Gtk.Label()
+        self.lyrics.set_justify(Gtk.Justification.CENTER)
+        self.lyrics.set_property("margin_left", 40)
+        self.lyrics.set_property("margin_right", 40)
+        self.lyrics.set_line_wrap(True)
 
-		lyrics_vbox.pack_start(self.title, False, False, 5)
-		lyrics_vbox.pack_start(self.spinner, False, False, 5)
-		lyrics_vbox.pack_start(self.lyrics, False, False, 5)
-		lyrics_vbox.set_size_request(350, 700)
+        self.spinner = Gtk.Spinner()
 
-		return lyrics_vbox
+        lyrics_vbox.pack_start(self.title, False, False, 5)
+        lyrics_vbox.pack_start(self.spinner, False, False, 5)
+        lyrics_vbox.pack_start(self.lyrics, False, False, 5)
+        lyrics_vbox.set_size_request(350, 700)
 
-	def put_lyrics(self, song):
-		self.spinner.start()
+        return lyrics_vbox
 
-		self.lyrics.set_text("")
-		lyrics = get_lyrics(song)
-		self.lyrics.set_text(lyrics)
+    def put_lyrics(self, song):
+        self.spinner.start()
 
-		self.spinner.stop()
+        self.lyrics.set_text("")
+        lyrics = get_lyrics(song)
+        self.lyrics.set_text(lyrics)
 
-	def fetch_lyrics(self, source=None):
-		input = self.input.get_text()
-		text = "<b><big>"+input+"</big></b>"
-		self.title.set_markup(text)
+        self.spinner.stop()
 
-		thread = threading.Thread(target=self.put_lyrics, kwargs={'song':input})
-		thread.daemon = True
-		thread.start()
+    def fetch_lyrics(self, source=None):
+        input = self.input.get_text()
+        text = "<b><big>" + input + "</big></b>"
+        self.title.set_markup(text)
 
-	def get_spotify_song_data(self):
-		session_bus = dbus.SessionBus()
-		
-		spotify_bus = session_bus.get_object("org.mpris.MediaPlayer2.spotify","/org/mpris/MediaPlayer2")
-		spotify_properties = dbus.Interface(spotify_bus,"org.freedesktop.DBus.Properties")
-		metadata = spotify_properties.Get("org.mpris.MediaPlayer2.Player", "Metadata")
-	
-		title = metadata['xesam:title'].encode('utf-8').decode('utf-8').replace("&", "&amp;")
-		artist = metadata['xesam:artist'][0].encode('utf-8').decode('utf-8').replace("&", "&amp;")
-		return {'title':title, 'artist':artist}
+        thread = threading.Thread(
+            target=self.put_lyrics, kwargs={'song': input})
+        thread.daemon = True
+        thread.start()
 
-	def get_spotify(self):
+    def get_spotify_song_data(self):
+        session_bus = dbus.SessionBus()
 
-		try:
-			song_data = self.get_spotify_song_data()
-			song = song_data['title']
-			artist = song_data['artist']
-		except:
-			self.title.set_markup("<big><b>Error</b></big>")
-			message = ("Could not get current spotify song\n"
-						"Either spotify is not running or\n"
-						"no song is playing on spotify.\n\n"
-						"Else, report an issue " \
-						"<a href=\"https://github.com/bhrigu123/Instant-Lyrics\" "
-                 "title=\"Repo url\">here</a>")
+        spotify_bus = session_bus.get_object(
+            "org.mpris.MediaPlayer2.spotify", "/org/mpris/MediaPlayer2")
+        spotify_properties = dbus.Interface(
+            spotify_bus, "org.freedesktop.DBus.Properties")
+        metadata = spotify_properties.Get(
+            "org.mpris.MediaPlayer2.Player", "Metadata")
 
-			self.lyrics.set_markup(message)
-			return
+        title = metadata['xesam:title'].encode(
+            'utf-8').decode('utf-8').replace("&", "&amp;")
+        artist = metadata['xesam:artist'][0].encode(
+            'utf-8').decode('utf-8').replace("&", "&amp;")
+        return {'title': title, 'artist': artist}
 
-		title = "<b><big>" + song + "</big>\n" + artist + "</b>"
-		self.title.set_markup(title)
+    def get_spotify(self):
 
-		self.put_lyrics(song + " " + artist)
+        try:
+            song_data = self.get_spotify_song_data()
+            song = song_data['title']
+            artist = song_data['artist']
+        except:
+            self.title.set_markup("<big><b>Error</b></big>")
+            message = ("Could not get current spotify song\n"
+                       "Either spotify is not running or\n"
+                       "no song is playing on spotify.\n\n"
+                       "Else, report an issue <a href=\"https://"
+                       "github.com/bhrigu123/Instant-Lyrics\" "
+                       "title=\"Repo url\">here</a>")
+
+            self.lyrics.set_markup(message)
+            return
+
+        title = "<b><big>" + song + "</big>\n" + artist + "</b>"
+        self.title.set_markup(title)
+
+        self.put_lyrics(song + " " + artist)
 
 
 class AppIndicator():
-	
-	def __init__(self):
-		signal.signal(signal.SIGINT, signal.SIG_DFL)
 
-		indicator = appindicator.Indicator.new(APPINDICATOR_ID, get_icon_path('icon.svg'), appindicator.IndicatorCategory.SYSTEM_SERVICES)
-		indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
-		indicator.set_menu(self.build_menu())
-		Gtk.main()
-	
-	def build_menu(self):
-		menu = Gtk.Menu()
+    def __init__(self):
+        signal.signal(signal.SIGINT, signal.SIG_DFL)
 
-		get_lyrics = Gtk.MenuItem('Get Lyrics')
-		get_lyrics.connect('activate', self.fetch_lyrics)
+        indicator = appindicator.Indicator.new(APPINDICATOR_ID, get_icon_path(
+            'icon.svg'), appindicator.IndicatorCategory.SYSTEM_SERVICES)
+        indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
+        indicator.set_menu(self.build_menu())
+        Gtk.main()
 
-		spotify_lyrics = Gtk.MenuItem('Spotify Lyrics')
-		spotify_lyrics.connect('activate', self.spotify_lyrics)
+    def build_menu(self):
+        menu = Gtk.Menu()
 
-		item_quit = Gtk.MenuItem('Quit')
-		item_quit.connect('activate', self.quit)
+        get_lyrics = Gtk.MenuItem('Get Lyrics')
+        get_lyrics.connect('activate', self.fetch_lyrics)
 
-		
-		menu.append(get_lyrics)
-		menu.append(spotify_lyrics)
-		menu.append(item_quit)
-		menu.show_all()
-		return menu
-	 
-	def fetch_lyrics(self, source):
-		win = LyricsWindow("get")
+        spotify_lyrics = Gtk.MenuItem('Spotify Lyrics')
+        spotify_lyrics.connect('activate', self.spotify_lyrics)
 
-	def spotify_lyrics(self, source):
-		win = LyricsWindow("spotify")
-		thread = threading.Thread(target=win.get_spotify)
-		thread.daemon = True
-		thread.start()
+        item_quit = Gtk.MenuItem('Quit')
+        item_quit.connect('activate', self.quit)
 
-	def quit(self, source):
-		Gtk.main_quit()
+        menu.append(get_lyrics)
+        menu.append(spotify_lyrics)
+        menu.append(item_quit)
+        menu.show_all()
+        return menu
+
+    def fetch_lyrics(self, source):
+        win = LyricsWindow("get")
+
+    def spotify_lyrics(self, source):
+        win = LyricsWindow("spotify")
+        thread = threading.Thread(target=win.get_spotify)
+        thread.daemon = True
+        thread.start()
+
+    def quit(self, source):
+        Gtk.main_quit()
+
 
 if __name__ == "__main__":
-	app = AppIndicator()
+    app = AppIndicator()
--- Lyrics.py
+++ Lyrics.py
@@ -3,35 +3,48 @@ from bs4 import BeautifulSoup
 import os
 
 try:
-	from urllib.parse import quote_plus
+    from urllib.parse import quote_plus
 except ImportError:
-	from urllib import quote_plus
+    from urllib import quote_plus
+
 
 def get_lyrics(song_name):
 
-	song_name +=  ' metrolyrics'
-	name =  quote_plus(song_name)
-	hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
-       'Accept-Language': 'en-US,en;q=0.8',
-       'Connection': 'keep-alive'}
+    song_name += ' metrolyrics'
+    name = quote_plus(song_name)
+    hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11'
+           '(KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+           'Accept-Language': 'en-US,en;q=0.8',
+           'Connection': 'keep-alive'}
 
-	url = 'http://www.google.com/search?q='+name
+    url = 'http://www.google.com/search?q=' + name
 
-	result = requests.get(url, headers=hdr).text
-	link_start=result.find('http://www.metrolyrics.com')
-	link_end=result.find('html',link_start+1)
-	link = result[link_start:link_end+4]
+    result = requests.get(url, headers=hdr).text
+    link_start = result.find('http://www.metrolyrics.com')
 
-	lyrics_html = requests.get(link, headers={'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.95 Safari/537.36'}).text
-	soup = BeautifulSoup(lyrics_html, "lxml")
-	raw_lyrics= (soup.findAll('p', attrs={'class' : 'verse'}))
-	paras=[]
-	try:
-		final_lyrics=unicode.join(u'\n',map(unicode,raw_lyrics))
-	except NameError:
-		final_lyrics=str.join(u'\n',map(str,raw_lyrics))
-	
-	final_lyrics= (final_lyrics.replace('<p class="verse">','\n'))
-	final_lyrics= (final_lyrics.replace('<br/>',' '))
-	final_lyrics = final_lyrics.replace('</p>',' ')
-	return (final_lyrics)
+    if(link_start == -1):
+        return("Lyrics not found on Metrolyrics")
+        
+    link_end = result.find('html', link_start + 1)
+    link = result[link_start:link_end + 4]
+
+
+    lyrics_html = requests.get(link, headers={
+                               'User-Agent': 'Mozilla/5.0 (Macintosh; Intel'
+                               'Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, '
+                               'like Gecko) Chrome/55.0.2883.95 Safari/537.36'
+                               }
+                               ).text
+
+    soup = BeautifulSoup(lyrics_html, "lxml")
+    raw_lyrics = (soup.findAll('p', attrs={'class': 'verse'}))
+    paras = []
+    try:
+        final_lyrics = unicode.join(u'\n', map(unicode, raw_lyrics))
+    except NameError:
+        final_lyrics = str.join(u'\n', map(str, raw_lyrics))
+
+    final_lyrics = (final_lyrics.replace('<p class="verse">', '\n'))
+    final_lyrics = (final_lyrics.replace('<br/>', ' '))
+    final_lyrics = final_lyrics.replace('</p>', ' ')
+    return (final_lyrics)
